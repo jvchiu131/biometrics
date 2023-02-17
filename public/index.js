f1038a7b41309607bd9fd072f0ab38c0649d684d
jvchiu131 <85332493+jvchiu131@users.noreply.github.com>
2023-02-18 02:50:04 +0800

    display built on for loop

--- public/index.js
+++ public/index.js
@@ -2,12 +2,21 @@ document.getElementById("file").addEventListener('change', function(){
 
     var file_scanner = new FileReader();
 
+    var file_screen = document.getElementById("filescreen");
+
+    var file = this.files[0];
+
     file_scanner.onload = function(){
-        document.getElementById("filescreen").textContent=file_scanner.result;
-    }
 
-    file_scanner.readAsText(this.files[0]);
+        var file_array = file_scanner.result.split(' ');
 
+       for(var line = 0; line < file_array.length - 1; line++){
+           //console.log(file_array[line])
+           file_screen.innerHTML = file_array;
+          
+       }
+    };
+    file_scanner.readAsText(file);
 
 })
 
